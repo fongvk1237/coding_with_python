39bcdc6a823e3edf0043c0be0c37fa822a2ec907
fong-test <107195734+fongvk1237@users.noreply.github.com>
2025-09-16 22:24:53 +0700

    summary

--- discovery_piscine/cell01/ex00/name.py
+++ discovery_piscine/cell01/ex00/name.py
@@ -1,4 +1,4 @@
 first_name = "Warunyu"
-last_name = "Threepoom"
+last_name  ="Threepoom"
 
 print(first_name, last_name)
---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Warunyu"
+last_name  ="Threepoom"
+
+whole_name = first_name + " " + last_name
+
+print(whole_name)
