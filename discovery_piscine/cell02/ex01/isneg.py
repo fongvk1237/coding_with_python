6dbb5c79b06a77cec10dade4eeb4cb99bf53f658
fong-test <107195734+fongvk1237@users.noreply.github.com>
2025-09-16 23:39:24 +0700

    summary

---
+++ discovery_piscine/cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+type_number = int(input("Enter a number: "))
+
+if type_number < 0:
+    print ("This number is negative.")
+elif type_number == 0:
+    print ("This number is both positive and negative.")
+else:
+    print ("This number is positive.")
---
+++ discovery_piscine/cell02/ex02/password.py
@@ -0,0 +1,7 @@
+your_password = input("Enter the password: ")
+password = "Python is awesome"
+
+if your_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
