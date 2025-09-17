8d7c3597a1eae311302ce14fbc6d22b94a74c790
fong-test <107195734+fongvk1237@users.noreply.github.com>
2025-09-17 14:13:41 +0700

    Create age.py

---
+++ discovery_piscine/cell04/ex01/age.py
@@ -0,0 +1,6 @@
+user = int(input("Please tell me your age: "))
+
+print("You are currently",user,"years old.")
+print("In 10 years, you'll be", user + 10, "years old.")
+print("In 10 years, you'll be", user + 20, "years old.")
+print("In 10 years, you'll be", user + 30, "years old.")
