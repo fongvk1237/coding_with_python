7c2abccf872e02628b6bd403e9218170bd1ab7d5
fong-test <107195734+fongvk1237@users.noreply.github.com>
2025-09-17 11:38:38 +0700

    Create to25.py

---
+++ discovery_piscine/cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+user = int(input("Enter a number less than 25 : "))
+
+if user > 25 :
+    print("Error")
+else :
+    while user <= 25 :
+        print("Inside the loop, my variable is " , user)
+        user += 1
